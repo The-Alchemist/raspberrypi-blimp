a9d421a0ceb48b4e13df38ba38b8ddcb97da0ece
The Alchemist <kap4020@gmail.com>
2018-03-03 15:15:21 -0500

    Fixing import after switching packages

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var sleep= require('sleep');
-var wpi = require('wiring-pi');
+var wpi = require('wiringpi-node');
 
 wpi.setup('wpi');
 
